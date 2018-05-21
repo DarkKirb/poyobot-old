19510bd5be8e64608f7c0c704187f49e7b2094d4
Dark Kirb <darkkirb@gmail.com>
2018-05-21 08:27:21 +0100

    fixed a bug with timestamps and added support for archiving a different channel and without images

--- mod/archiver.py
+++ mod/archiver.py
@@ -6,6 +6,7 @@ import os
 import io
 import hashlib
 import datetime
+import discord
 from .tar import TARInstance
 
 
@@ -19,10 +20,13 @@ class Archiver(Cog):
     dependencies = ["tar"]
 
     @command()
-    async def archiver(self, ctx):
+    async def archiver(self, ctx, channel: discord.TextChannel = None,
+                       include_images: bool = True):
         """Archives the current channel (but doesn't delete it)"""
+        if channel is None:
+            channel = ctx.message.channel
         if not await is_mod(ctx.message.guild, ctx.message.author,
-                            ctx.message.channel):
+                            channel):
             await ctx.send("You need to be moderator to archive a channel!")
         msg = await ctx.send("Archiving the channel. This might take a while")
         number = 0
@@ -50,36 +54,37 @@ class Archiver(Cog):
                 for line in message.content.split("\n")[1:]:
                     await f.write(f"{padding} {line}\n")
 
-                for attachment in message.attachments:
-                    f2 = io.BytesIO()
-                    await attachment.save(f2)
-                    filehash = hashlib.sha256(f2.read()).hexdigest()
-                    fsize = f2.tell()
-                    f2.seek(0)
-                    ext = attachment.filename.rpartition('.')[2]
-                    await f.write(
-                        f"{padding} Attachment " +
-                        f"{attachment.filename} {attachment.url} ")
-                    if fsize < 8*1024*1024 - 4096:
-                        fname = os.path.join("imgs", f"{filehash}.{ext}")
-                        async with tar.open(fname, "wb") as f3:
-                            await f3.write(f2.read())
-                        await f.write(f"(saved as {fname})\n")
-                    else:
-                        await f.write(f"(too large to be saved 🙁)\n")
-                    f2 = None  # save memory
+                if include_images:
+                    for attachment in message.attachments:
+                        f2 = io.BytesIO()
+                        await attachment.save(f2)
+                        filehash = hashlib.sha256(f2.read()).hexdigest()
+                        fsize = f2.tell()
+                        f2.seek(0)
+                        ext = attachment.filename.rpartition('.')[2]
+                        await f.write(
+                            f"{padding} Attachment " +
+                            f"{attachment.filename} {attachment.url} ")
+                        if fsize < 8*1024*1024 - 4096:
+                            fname = os.path.join("imgs", f"{filehash}.{ext}")
+                            async with tar.open(fname, "wb") as f3:
+                                await f3.write(f2.read())
+                            await f.write(f"(saved as {fname})\n")
+                        else:
+                            await f.write(f"(too large to be saved 🙁)\n")
+                        f2 = None  # save memory
 
-                for embed in message.embeds:
-                    await f.write(f"{padding} EMBED: {embed.to_dict()}\n")
+                    for embed in message.embeds:
+                        await f.write(f"{padding} EMBED: {embed.to_dict()}\n")
                 number += 1
                 if not number % 250:
                     await msg.edit(content=f"Archived {number} messages…\n")
 
             found = True
-            message_ts = datetime.datetime.fromtimestamp(0)
+            message_ts = datetime.datetime.fromtimestamp(1420070400)
             while found:
                 found = False
-                async for message in ctx.message.channel.history(
+                async for message in channel.history(
                         limit=None,
                         reverse=True,
                         after=message_ts):
